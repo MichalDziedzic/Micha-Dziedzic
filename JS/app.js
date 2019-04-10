8012731f42bf26f840037f50ad64fa8bc576240e
MichalDziedzic <michaldziedzic8@gmail.com>
2019-04-10 22:19:36 +0200

    ver10.04

--- JS/app.js
+++ JS/app.js
@@ -1,53 +1,91 @@
 
-let addTaskButton= null;
-let gripSection=null;
+document.addEventListener("DOMContentLoaded", function () {
+
+  const addTaskButton = document.querySelector('#addTaskButton');
+  const gripSection = document.querySelector('.section');
+  const modal=document.querySelector('.modal-block');
+  const cancel=document.querySelector('.cancel-x');
+  const openModal=document.querySelector('#openModal');
+  const todoForm=document.querySelector('.todoForm');
+
+  openModal.addEventListener('click', openmodal);
+  cancel.addEventListener('click',closemodal);
+  addTaskButton.addEventListener('click',closemodal);
+  todoForm.addEventListener('submit',function(e){
+
+    console.log('jest w funcji!');
+    e.preventDefault();
+      const Textarea = this.querySelector('Textarea');
+      const TaskName = this.querySelector('#NameTask');
+      if(Textarea.value !=='' && TaskName.value !==''){
+        AddTask(TaskName.value,Textarea.value);
+        Textarea.value='';
+        TaskName.value='';
+        
+      }
+  });
+
+ 
+
+  function addClassToElem(elem, array) {
+
+    for (var i = 0; array.length > i; i++) {
+
+      elem.classList.add(array[i]);
+
+    }
 
 
-document.addEventListener("DOMContentLoaded",function(){
-    
-  const addTaskButton=document.querySelector('#addTaskButton');
-  const gripSection=document.querySelector('#section');  
+  }
+
+  function openmodal(){
 
     
-    
+   modal.style.setProperty('display','block');
 
-    addTaskButton.addEventListener('click',AddTask,false);
-    
-    
-    
-    
-    
 
-    
+  }
+  function closemodal(){
+
+    modal.style.setProperty('display','none');
+  }
+
+  function AddTask(name,text) {
+    console.log('dsgdsgsdf')
+
+    const todo = document.createElement('div');
+
+    addClassToElem(todo, ['row', 'section', 'd-flex', 'justify-content-center']);
+
+    const todoBar = document.createElement('div');
+    //todoBar.classList.add('col-6 row section-cards section-card text-white bg-info mb-3');
+    addClassToElem(todoBar, ['col-6', 'row', 'section-cards', 'section-card', 'text-white', 'bg-info', 'mb-3'])
+
+    const todoBarsecond = document.createElement('div');
+    //todoBarsecond.classList.add('card-header header-card');
+    addClassToElem(todoBarsecond, ['card-header', 'header-card']);
+    todoBarsecond.innerText=name;
+
+    const todoBarthird = document.createElement('div');
+    todoBarthird.classList.add('card-body');
+
+
+    const todoBarfour = document.createElement('div');
+    todoBarfour.classList.add('card-text');
+    todoBarfour.innerText =Text;
+
+    todo.appendChild(todoBar);
+    todoBar.appendChild(todoBarsecond);
+    todoBarsecond.appendChild(todoBarthird);
+    todoBarthird.appendChild(todoBarfour);
+    gripSection.append(todo);
+
+
+  }
+
+
+
+
+
 });
-
-
-function AddTask()
-{
-
-    const todo =document.createElement('div');
-    todo.classList.add("row  section d-flex justify-content-center"); 
-    
-
-                const todoBar = document.createElement('div');
-                todoBar.classList.add('col-6 row section-cards section-card text-white bg-info mb-3');
-
-                const todoBarsecond = document.createElement('div');
-                todoBarsecond.classList.add('card-header header-card');
-
-                const todoBarthird = document.createElement('div');
-                todoBarthird.classList.add('card-body');
-                
-                const todoBarfour = document.createElement('div');
-                todoBarfour.classList.add('card-text');
-                todoBarfour.innerText="test";
-
-        todo.appendChild(todoBar);
-        todoBar.appendChild(todoBarsecond);
-        todoBarsecond.appendChild(todoBarthird);
-        todoBarthird.appendChild(todoBarfour);
-        todo.append(gripSection);
-
-
-}
 
