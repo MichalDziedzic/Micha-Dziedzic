c34afe1157964dcaeb26fb5f782174e29f77c6a0
MichalDziedzic <michaldziedzic8@gmail.com>
2019-04-25 14:43:46 +0200

    ver25.04.01

--- JS/app.js
+++ JS/app.js
@@ -30,14 +30,17 @@ firebase.auth().onAuthStateChanged(function (user) {
 });
 
   const addTaskButton = document.querySelector('#addTaskButton');
-  const gripSection = document.querySelector('.section');
+  const gripSection = document.querySelector('.sectionAll');
   const modal = document.querySelector('.modal-block');
   const cancel = document.querySelector('.cancel-x');
   const openModal = document.querySelector('#openModal');
   const todoForm = document.querySelector('.todoForm');
   const signInn = document.querySelector('.sign-in');
   const logout = document.querySelector('#logout');
-
+  const cancelBtn=document.querySelector('.cancel-btn');
+ 
+  
+ 
   logout.addEventListener('click',flogOut);
   openModal.addEventListener('click', openmodal);
   cancel.addEventListener('click', closemodal);
@@ -51,8 +54,14 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 
   });
+                                    
 
+  gripSection.addEventListener('click',function(e){
   
+  if(e.target.closest('.cancel-btn')!==null){
+    
+    e.target.closest('.card-area').remove();
+}});
 
 
 
@@ -122,6 +131,22 @@ function addUserDescribe(username){
 
 
 }
+  
+    //const deadDate= document.querySelector('#deadlinedata');
+    //const deadTime=document.querySelector('#deadlinetime');
+  /*if(deadDate!==null)
+  {
+    deadDate.addEventListener('onchange',function(){
+
+    var inputDate = deadDate.value; 
+     var dateshow = new Date(inputDate);
+      console.log(dateshow.getFullYear());
+
+
+    });
+  }
+
+*/
 
   todoForm.addEventListener('submit', function (e) {
 
@@ -129,26 +154,52 @@ function addUserDescribe(username){
     e.preventDefault();
     const Textarea = this.querySelector('Textarea');
     const TaskName = this.querySelector('#NameTask');
+    var deadDate= document.querySelector('#deadlinedate');
+    var deadTime=document.querySelector('#deadlinetime');
+    
+   
     if (Textarea.value !== '' && TaskName.value !== '') {
-      AddTask(TaskName.value, Textarea.value);
-      Textarea.value = '';
-      TaskName.value = '';
 
+     
+            AddTask(TaskName.value, Textarea.value , deadDate.value ,deadTime.valueAsDate);
+          
+            console.log('przeszlo');
+            
+            
+           
+            
+           // console.log(inputTime+":"+(inputTime.getHours()-1));
+
+
+            Textarea.value = '';
+            TaskName.value = '';
+            deadDate.value='';
+
+
+           
+      
+    
     }
   });
 
+//function timeprint(dateform,timeform){
+
+  
 
 
-  function addClassToElem(elem, array) {
+//}
 
-    for (var i = 0; array.length > i; i++) {
+  function addClassToElem(elem, array){
 
-      elem.classList.add(array[i]);
+    
+    for (var i = 0; array.length > i; i++)
+      {
+        elem.classList.add(array[i]);
 
-    }
-
+      }
 
   }
+  
 
   function openmodal() {
 
@@ -161,9 +212,15 @@ function addUserDescribe(username){
 
     modal.style.setProperty('display', 'none');
   }
+  
+  
+  
 
-  function AddTask(name, textfromarea) {
-    console.log('dsgdsgsdf')
+  function AddTask(name,textfromarea,inputDate,inputTime) {
+    
+
+
+    var dateshow = new Date(inputDate);
 
     const todo = document.createElement('div');
 
@@ -171,30 +228,62 @@ function addUserDescribe(username){
 
     const todoBar = document.createElement('div');
     //todoBar.classList.add('col-6 row section-cards section-card text-white bg-info mb-3');
-    addClassToElem(todoBar, ['col-6', 'row', 'section-cards', 'section-card', 'text-white', 'bg-info', 'mb-3'])
+    //col-12 col-sm-12 col-md-6 col-lg-6
+    addClassToElem(todoBar, ['row','col-sm-12','col-md-6','col-12','col-lg-6','section-cards', 'section-card', 'text-white', 'bg-info', 'mb-3'])
 
     const todoBarsecond = document.createElement('div');
     //todoBarsecond.innerHTML=name + "</div>";
 
     //todoBarsecond.classList.add('card-header header-card');
-    addClassToElem(todoBarsecond, ['card-header', 'header-card']);
+    addClassToElem(todoBarsecond, ['card-header', 'header-card','d-flex','justify-content-between']);
     todoBarsecond.innerText = name;
 
+    const buttonsblock=document.createElement('div');
+    buttonsblock.classList.add('buttons');
 
     const todoBarthird = document.createElement('div');
     todoBarthird.classList.add('card-body');
 
+    const cardArea=document.createElement('div');
+    cardArea.classList.add('card-area');
 
     const todoBarfour = document.createElement('p');
     todoBarfour.classList.add('card-text');
     todoBarfour.innerText = textfromarea;
 
+   // createbuttons(['btnone','btntwo','btnthree'],['btn','btn-info','edit'],['btn','btn-info','cancel-btn']);
+
+    const createbtnone=document.createElement('button');
+    const createbtntwo=document.createElement('button');
+    addClassToElem(createbtnone,['btn','btn-info','edit']);
+    addClassToElem(createbtntwo,['btn','btn-info','cancel-btn']);
+
+    const createione=document.createElement('i');
+    const createitwo=document.createElement('i');
+
+    addClassToElem(createione,['far','fa-edit']);
+    addClassToElem(createitwo,['far','fa-times-circle']);
+
+    
+    const dateDeadline=document.createElement('div');
+    addClassToElem(dateDeadline,['card-header','date-deadline']);
+    dateDeadline.innerText="Deadline : "+dateshow.getDay()+"/"+dateshow.getMonth()+"/"+dateshow.getFullYear()+" "+(inputTime.getHours()-1)+":"+inputTime.getMinutes();
+    cardArea.append(todo) 
     todo.appendChild(todoBar);
     todoBar.appendChild(todoBarsecond);
     todoBar.appendChild(todoBarthird);
     todoBarthird.appendChild(todoBarfour);
-    // todoBar.append(todoBarsecond); 
-    gripSection.append(todo); // dzieki 
+    // todoBar.append(todoBarsecond);
+    todoBarsecond.append(buttonsblock);
+    gripSection.append(cardArea);
+    buttonsblock.append(createbtnone);
+    buttonsblock.append(createbtntwo);
+    createbtnone.append(createione);
+    createbtntwo.append(createitwo);
+    todoBarthird.append(dateDeadline);
+      
+
+    //gripSection.append(todo); // dzieki 
 
 
   }
